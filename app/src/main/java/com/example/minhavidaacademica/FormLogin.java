48d397f1971cd0b02a7328820f28f2a324c79ef1
Sarubi <carlossarubiuni@gmail.com>
2022-03-22 15:02:50 -0300

    Start Commit

--- app/src/main/java/com/example/minhavidaacademica/FormLogin.java
+++ app/src/main/java/com/example/minhavidaacademica/FormLogin.java
@@ -3,14 +3,22 @@ package com.example.minhavidaacademica;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class FormLogin extends AppCompatActivity {
 
+    private EditText edit_email, edit_senha;
+    private Button bt_entrar;
     private TextView link;
+    String[] mensagens = {"Preencha todos os campos"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +33,29 @@ public class FormLogin extends AppCompatActivity {
             public void onClick(View view) {
                 Intent Intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse("http://www.minhavidaacademica.com.br/View/quem_cadastrar.php"));
                 startActivity(Intent);
-
             }
         });
 
+        IniciarComponentes();
+        bt_entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = edit_email.getText().toString();
+                String senha = edit_senha.getText().toString();
+
+                if(email.isEmpty() || senha.isEmpty()){
+                    Snackbar snackbar = Snackbar.make(view, mensagens[0], Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }
+            }
+        });
+    }
+
+    private void IniciarComponentes(){
+        edit_email = findViewById(R.id.edit_email);
+        edit_senha = findViewById(R.id.edit_senha);
+        bt_entrar = findViewById(R.id.bt_entar);
     }
 }
